404699dc2aa574735f985497ad22bab6424f94a3
iamodan <rurii@knou.ac.kr>
2024-07-05 10:47:44 +0900

    스프링부트 강좌 22강(블로그 프로젝트) - 연관관계의 주인

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,18 +1,22 @@
 package com.cos.blog.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -39,15 +43,14 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 
-	//	@ManyToOne(fetch = FetchType.EAGER)  // Many = Many,  User = One
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)  // Many = Many,  User = One
 	@JoinColumn(name = "userId")
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다. 
 
-	//	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
-	//	@JsonIgnoreProperties({"board"})
-	//	@OrderBy("id desc")
-	//	private List<Reply> replys;
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+//	@JsonIgnoreProperties({"board"})
+//	@OrderBy("id desc")
+	private List<Reply> replys;
 
 	@CreationTimestamp
 	private LocalDateTime createDate;
