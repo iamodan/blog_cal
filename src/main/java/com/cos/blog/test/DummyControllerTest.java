9ae1334da6ead4db06572d104c207c74cb8ce794
iamodan <rurii@knou.ac.kr>
2024-07-08 09:37:19 +0900

    스프링부트 강좌 26강(블로그 프로젝트) - id로 select 테스트

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +12,37 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
-// html파일이 아니라 data를 리턴해주는 controller = RestController
+// **html파일이 아니라 data를 리턴**해주는 controller = RestController
 @RestController
 public class DummyControllerTest {
 	
 	@Autowired
 	private UserRepository userRepository;
 
+	// http://localhost:8000/blog/dummy/user/5
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		
+//		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {
+//			@Override
+//			public User get() {
+//				// TODO Auto-generated method stub
+//				return new User();
+//			}
+//		});	
+//		return user;
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("該当するユーザーがございません。　id : "+id);
+			}
+		});
+		return user;
+	}
+	
 	@PostMapping("/dummy/join")
 	public String join(User user) {
 //		System.out.println("id :::   " + user.getId());
