8d486db8e351f0be4669ff55ba7900d90f8fec79
iamodan <rurii@knou.ac.kr>
2024-07-12 14:37:32 +0900

    스프링부트 강좌 38강(블로그 프로젝트) - 회원가입 하기 Ajax요청

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,6 +1,7 @@
 let index = {
 	init: function() {
 		$("#btn-save").on("click", () => {
+			//()=>{} } ::: thisを　bindingする為！！
 			this.save();
 		});
 	},
@@ -17,8 +18,19 @@ let index = {
 		// 데이터 값을 로그에 출력
 		//		console.log(data);
 
-
-		$.ajax().done().fail();
+		$.ajax({
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data), // http body데이터
+			contentType:"application/json; charset=utf-8", //body데이터가 어떤 타입인지
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본l적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
+		}).done(function(resp) {
+			alert("회원가입이 완료되었습니다.");
+			console.log(resp)
+//			location.href = "/blog";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
 		//ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청!
 	}
 }
