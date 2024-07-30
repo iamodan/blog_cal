a28b357145876637f2a52fc7ee1d6397769e3661
unknown <rurii@knou.com>
2024-07-31 00:42:41 +0900

    스프링부트 강좌 60강(블로그 프로젝트) - 회원수정 1

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -2,13 +2,12 @@ package com.cos.blog.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.dto.ResponseDto;
-import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 
@@ -26,7 +25,10 @@ public class UserApiController {
 		userService.会員登録(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
-
-
-
+	
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user){
+		userService.会員修正(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
 }
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -28,6 +28,24 @@ public class UserService {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
+
+	@Transactional
+	public void 会員修正(User user) {
+		// 수정시에는 영속성 컨텍스트 User 오브젝트를 영속화시키고, 영속화된 User 오브젝트를 수정
+		// select를 해서 User오브젝트를 DB로 부터 가져오는 이유는 영속화를 하기 위해서!!
+		// 영속화된 오브젝트를 변경하면 자동으로 DB에 update문을 날려줌!!!
+		User persistance = userRepository.findById(user.getId()).orElseThrow(() -> {
+			return new IllegalArgumentException("会員探し失敗");
+		});
+		String rawPassword = user.getPassword();
+		String encPassword = encoder.encode(rawPassword);
+		persistance.setPassword(encPassword);
+		persistance.setEmail(user.getEmail());
+		// 회원 수정 함수 종료 시 = 서비스 종료 = 트랜젝션 종료 = commit이 자동으로 됩니다.
+		// 영속화된 persistace 객체의 변화가 감지되면 더티체킹이 되어 update문을 날려줌
+	}
+
+
 	
 //	@Transactional(readOnly = true) // Select할 떄 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료
 //	public User ログイン(User user) {
