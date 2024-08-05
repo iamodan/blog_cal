42bd4790dd2562d5118d128bafd43f55c3a70a49
unknown <rurii@knou.com>
2024-08-06 00:51:31 +0900

    2024/08/05

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,12 +1,23 @@
 package com.cos.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.model.KakaoProfile;
+import com.cos.blog.model.OAuthToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 //인증이 안된 사용자들이 출입할 수 있는 경로를/auth/**허용
 // 그냥 주소가 /이면 index.jsp허용
@@ -28,7 +39,82 @@ public class UserController {
 
 	@GetMapping("/auth/kakao/callback")
 	public @ResponseBody String kakaoCallback(String code) { // Data를 리턴해주는 컨트롤러 함수
-		return "カカオ引証コード値 :::" + code;
+
+		// POST방식으로 Key=value 데이터를 요청(카카오쪽)으로
+
+		/*
+		 * d https://kauth.kakao.com/oauth/token grant_type=authorization_code
+		 * client_id=a6dc7c061bba38ecf26964ea6933d38e
+		 * redirect_uri=http://localhost:8080/auth/kakao/callback code=｛動的→今、分からない｝
+		 * client_secret=必須ｘ
+		 */
+
+		RestTemplate rt = new RestTemplate();
+
+		// HTTpHeader Object 生成
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		// HTTpBody Object 生成
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "a6dc7c061bba38ecf26964ea6933d38e");
+		params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+		params.add("code", code);
+
+		// ①HttpHeader ②HTTpBodyを一つのobjectにまとめる
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+		// Httpを送信する - POST方式で - そしてresponse変数に応答を受け取る
+		ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST, kakaoTokenRequest, String.class);
+
+		// Gson, Json Simple, Object Mapper
+		ObjectMapper objectMapper = new ObjectMapper();
+		OAuthToken oauthToken = null;
+
+		try {
+			oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("カカオ_Access_Token :" + oauthToken.getAccess_token());
+
+		RestTemplate rt2 = new RestTemplate();
+
+		// HTTpHeader Object 生成
+		HttpHeaders headers2 = new HttpHeaders();
+		
+		headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
+		headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		// ①HttpHeader ②HTTpBodyを一つのobjectにまとめる
+		HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 = new HttpEntity<>(headers2);
+
+		// Httpを送信する - POST方式で - そしてresponse変数に応答を受け取る
+		ResponseEntity<String> response2 = rt2.exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST, kakaoProfileRequest2, String.class);
+
+//		return"カカオ引証完了! token access requestに対するresponse ::: "+response;
+		
+		System.out.println(response2.getBody());
+		
+		ObjectMapper objectMapper2 = new ObjectMapper();
+		KakaoProfile kakaoProfile = null;
+
+		try {
+			kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("kakao id(no):" + kakaoProfile.getId());
+		System.out.println("kakao email:" + kakaoProfile.getKakao_account().getEmail());
+		
+		return response2.getBody();
 	}
 
 	@GetMapping("/user/updateForm")
